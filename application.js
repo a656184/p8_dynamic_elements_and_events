350e92dd3955897e18153b0ee0d3d1c5cbacf264
Eric Kristensen <eric.l.kristensen@gmail.com>
2013-08-20 23:18:55 -0700

    create js functions to add, delete, complete and clear todos

--- application.js
+++ application.js
@@ -2,13 +2,30 @@ $(document).ready(function() {
   var todoTemplate = $.trim($('#todo_template').html());
 
   function bindEvents() {
-    // Bind functions which add, remove, and complete todos to the appropriate
-    // elements
+    $('.add').on('click', addTodos);
+    $('.todo_list').on('click', '.delete', removeTodos);
+    $('.todo_list').on('click', '.complete', completeTodo)
   }
 
-  //Create functions to add, remove and complete todos
+  function addTodos () {
+    var todo = $('.todo').val()  
+    $(".todo_list").append(buildTodo(todo));
+    clearInput();
+  }
 
-  
+  function removeTodos (){  
+    $(this).closest('div').remove();
+  }
+
+  function completeTodo () {
+    $(this).closest('li').text("Completed")
+    $(this).remove();
+  }
+
+  function clearInput() {
+    $('.todo').val('');
+    $('.todo').focus();
+  }
 
   function buildTodo(todoName) {
     // Creates an jQueryDOMElement from the todoTemplate.
